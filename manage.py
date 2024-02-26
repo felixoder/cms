0f0119cae06b46ea71c8921603af2852f03944ba
Debayan <debayanghosh408@gmail.com>
2024-02-26 20:43:29 +0530

    s

--- manage.py
+++ manage.py
@@ -11,10 +11,11 @@ def main():
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
+        "Couldn't import Django. Are you sure it's installed and "
+        "available on your PYTHONPATH environment variable? Did you "
+        "forget to activate a virtual environment?"
+    )
+
     execute_from_command_line(sys.argv)
 
 
