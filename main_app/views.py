ac57f0d55a73311508fa688cda0a769fe8d478d6
Debayan <debayanghosh408@gmail.com>
2024-02-26 21:40:59 +0530

    removed capctha

--- main_app/views.py
+++ main_app/views.py
@@ -23,32 +23,14 @@ def login_page(request):
     return render(request, 'main_app/login.html')
 
 
+
 def doLogin(request, **kwargs):
     if request.method != 'POST':
         return HttpResponse("<h4>Denied</h4>")
     else:
-        #Google recaptcha
-        captcha_token = request.POST.get('g-recaptcha-response')
-        captcha_url = "https://www.google.com/recaptcha/api/siteverify"
-        captcha_key = "6LdovYApAAAAAFG2T9qaA7VDZcyQucclNELvW3wq"
-        data = {
-            'secret': captcha_key,
-            'response': captcha_token
-        }
-        # Make request
-        try:
-            captcha_server = requests.post(url=captcha_url, data=data)
-            response = json.loads(captcha_server.text)
-            if response['success'] == False:
-                messages.error(request, 'Invalid Captcha. Try Again')
-                return redirect('/')
-        except:
-            messages.error(request, 'Captcha could not be verified. Try Again')
-            return redirect('/')
-        
-        #Authenticate
+        # Authenticate
         user = EmailBackend.authenticate(request, username=request.POST.get('email'), password=request.POST.get('password'))
-        if user != None:
+        if user is not None:
             login(request, user)
             if user.user_type == '1':
                 return redirect(reverse("admin_home"))
@@ -59,8 +41,6 @@ def doLogin(request, **kwargs):
         else:
             messages.error(request, "Invalid details")
             return redirect("/")
-
-
 
 def logout_user(request):
     if request.user != None:
